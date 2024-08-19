13d7aeea0069b0292b2e282f8dbf5d92d3e68ca1
Singh Anubhav Gajendra <anubhav.gajendra@myntra.com>
2024-08-19 12:05:08 +0530

    respond 200 v2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -24,7 +24,7 @@ public class Main {
        PrintWriter  out = new PrintWriter(clientSocket.getOutputStream(), true);
        BufferedReader  in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
-       out.println("HTTP/1.1 200 OK\\r\\n\\r\\n\n");
+       out.print("HTTP/1.1 200 OK\\r\\n\\r\\n");
 
          System.out.println("accepted new connection");
      } catch (IOException e) {
