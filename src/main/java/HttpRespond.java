d1f1c5e7adc78f8cb9b3ba74f356852a0b41ca0e
Singh Anubhav Gajendra <anubhav.gajendra@myntra.com>
2024-08-21 11:19:48 +0530

    extract URL path

---
+++ src/main/java/HttpRespond.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class HttpRespond {
+    public static void main(String[] args) {
+        // You can use print statements as follows for debugging, they'll be visible when running tests.
+        System.out.println("Logs from your program will appear here!");
+        final int portNumber = 4221;
+//     Uncomment this block to pass the first stage
+
+        try (ServerSocket serverSocket = new ServerSocket(portNumber)) {
+
+            // Since the tester restarts your program quite often, setting SO_REUSEADDR
+            // ensures that we don't run into 'Address already in use' errors
+            serverSocket.setReuseAddress(true);
+
+            Socket clientSocket = serverSocket.accept(); // Wait for connection from client.
+
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+//       out.println("HTTP/1.1 200 OK\r\n\r");
+       clientSocket.getOutputStream().write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+
+
+            System.out.println("accepted new connection");
+        } catch (IOException e) {
+            System.out.println("IOException: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,32 +4,10 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.List;
 
 public class Main {
   public static void main(String[] args) {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    System.out.println("Logs from your program will appear here!");
-    final int portNumber = 4221;
-//     Uncomment this block to pass the first stage
-
-     try (ServerSocket serverSocket = new ServerSocket(portNumber)) {
-
-       // Since the tester restarts your program quite often, setting SO_REUSEADDR
-       // ensures that we don't run into 'Address already in use' errors
-       serverSocket.setReuseAddress(true);
-
-       Socket clientSocket = serverSocket.accept(); // Wait for connection from client.
-
-       PrintWriter  out = new PrintWriter(clientSocket.getOutputStream(), true);
-       BufferedReader  in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-
-//       out.println("HTTP/1.1 200 OK\r\n\r");
-       clientSocket.getOutputStream().write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-
-
-         System.out.println("accepted new connection");
-     } catch (IOException e) {
-       System.out.println("IOException: " + e.getMessage());
-     }
+    ExtractURLPath.main(args);
   }
 }
