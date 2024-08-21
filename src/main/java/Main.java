635be3d0d99d116c6ab76bfbab032efee756c80e
Singh Anubhav Gajendra <anubhav.gajendra@myntra.com>
2024-08-21 12:13:51 +0530

    respond with body

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 public class Main {
   public static void main(String[] args) {
-    ExtractURLPath.main(args);
+//    ExtractURLPath.main(args);
+    RespondWithBody.main(args);
   }
 }
--- src/main/java/RespondWithBody.java
+++ src/main/java/RespondWithBody.java
@@ -4,10 +4,11 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.text.MessageFormat;
 import java.util.HashMap;
 import java.util.Map;
 
-public class ExtractURLPath {
+public class RespondWithBody {
     public static void main(String[] args) {
         // You can use print statements as follows for debugging, they'll be visible when running tests.
         System.out.println("Logs from your program will appear here!");
@@ -17,6 +18,7 @@ public class ExtractURLPath {
         Map<String, String> response = new HashMap<>();
         response.put("ok", "HTTP/1.1 200 OK\r\n\r\n");
         response.put("notFound", "HTTP/1.1 404 Not Found\r\n\r\n");
+        response.put("echo", "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {0}\r\n\r\n{1}");
 
         try (ServerSocket serverSocket = new ServerSocket(portNumber)) {
 
@@ -34,6 +36,18 @@ public class ExtractURLPath {
 //            System.out.println(lines[1]);
             if(lines[1].equals("/")){
                 clientSocket.getOutputStream().write(response.get("ok").getBytes());
+            }
+            else if(lines[1].matches("^/echo/.*$")) {
+//                System.out.println("inside");
+
+                String[] path = lines[1].split("/");
+//                System.out.println(path[0]+" | "+path[1]+" | "+path[2]);
+                if((path.length==3) && (path[1].equals("echo"))) {
+                    String pathInput = path[2];
+                    String output = MessageFormat.format(response.get("echo"),pathInput.length(), pathInput);
+//                    System.out.println(output);
+                    clientSocket.getOutputStream().write(output.getBytes());
+                }
             }
             else {
                 clientSocket.getOutputStream().write(response.get("notFound").getBytes());
